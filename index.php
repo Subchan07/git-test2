0ef4810286275ee47ab94685477cef2c12a6b55d
Subchan07 <muchammadsubchan34@gmail.com>
2023-02-07 13:39:52 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
   <h1>Hello World</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus odit, natus quod consequatur beatae aliquam corporis tempore repudiandae possimus atque laborum velit? Nam mollitia vitae quam velit eligendi, magni possimus.</p>
+  <ul>
+    <li>Satu</li>
+    <li>Dua</li>
+  </ul>
 </body>
 </html>
